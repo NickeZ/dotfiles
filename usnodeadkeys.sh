6ef5609b0d98f5e8b21e2dcd120c22bbd75c9c29
Niklas Claesson <nicke.claesson@gmail.com>
2018-10-20 18:27:37 +0200

    no dead keys

---
+++ usnodeadkeys.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+patch -p1 -d '/' << 'EOF'
+--- a/usr/share/X11/xkb/symbols/us	2018-10-20 18:21:16.507009269 +0200
++++ b/usr/share/X11/xkb/symbols/us	2018-10-20 18:21:26.391023210 +0200
+@@ -144,6 +144,19 @@
+     include "level3(ralt_switch)"
+ };
+ 
++partial alphanumeric_keys
++xkb_symbols "intl_nodeadkeys" {
++
++    name[Group1]= "U.S. English - International (without plain dead keys)";
++
++    include "us(intl)"
++
++    // Alphanumeric section (overriding some annoying dead keys)
++    key <TLDE> { [     grave,       asciitilde ] };
++    key <AE06> { [    6, asciicircum,    onequarter,      asciicircum ] };
++    key <AC11> { [ apostrophe,        quotedbl ] };
++};
++
+ // Based on symbols/us_intl keyboard map:
+ // Dead-keys definition for a very simple US/ASCII layout.
+ // by Conectiva (http://www.conectiva.com.br)
+EOF
