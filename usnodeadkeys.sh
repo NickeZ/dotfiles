9003f6147e48ecbd1fb59d5e5f7dbfe090682110
Niklas Claesson <nicke.claesson@gmail.com>
2018-10-20 18:42:27 +0200

    us nodeadkeys

--- usnodeadkeys.sh
+++ usnodeadkeys.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-patch -p1 -d '/' << 'EOF'
---- a/usr/share/X11/xkb/symbols/us	2018-10-20 18:21:16.507009269 +0200
-+++ b/usr/share/X11/xkb/symbols/us	2018-10-20 18:21:26.391023210 +0200
+patch -p1 -d '/usr/share/X11/xkb/symbols' << 'EOF'
+--- a/us	2018-10-20 18:21:16.507009269 +0200
++++ b/us	2018-10-20 18:21:26.391023210 +0200
 @@ -144,6 +144,19 @@
      include "level3(ralt_switch)"
  };
@@ -23,4 +23,18 @@ patch -p1 -d '/' << 'EOF'
  // Based on symbols/us_intl keyboard map:
  // Dead-keys definition for a very simple US/ASCII layout.
  // by Conectiva (http://www.conectiva.com.br)
+EOF
+
+patch -p1 -d '/etc/default' << 'EOF'
+--- a/keyboard
++++ b/keyboard
+@@ -4,7 +4,7 @@
+ 
+ XKBMODEL="pc105"
+ XKBLAYOUT="us"
+-XKBVARIANT=""
++XKBVARIANT="intl_nodeadkeys"
+ XKBOPTIONS=""
+ 
+ BACKSPACE="guess"
 EOF
