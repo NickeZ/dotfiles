29ea49bcedaadbad87e069d3a16b5ad18be42f85
Niklas Claesson <niklas.claesson@klarna.com>
2020-04-09 14:22:27 +0200

    wip

--- usnodeadkeys.sh
+++ usnodeadkeys.sh
@@ -41,38 +41,43 @@ EOF
 
 # TODO: Update evdev.xml and evdev.lst in /usr/share/X11/xkb/rules/
 # TODO: Is below needed? probably not
-echo Now run, \`sudo dpkg-reconfigure keyboard-configuration\`
-echo or \`sudo dpkg-reconfigure xkb-data\`
+# echo Now run, \`sudo dpkg-reconfigure keyboard-configuration\`
+# echo or \`sudo dpkg-reconfigure xkb-data\`
 
-# --- evdev.lst.orig	2019-11-04 14:19:24.112470691 +0100
-# +++ evdev.lst	2019-11-04 14:20:24.040410777 +0100
-# @@ -295,6 +295,7 @@
-#    chr             us: Cherokee
-#    euro            us: English (US, euro on 5)
-#    intl            us: English (US, intl., with dead keys)
-# +  intl_nodeadkeys us: English (US, intl., with no dead keys)
-#    alt-intl        us: English (US, alt. intl.)
-#    colemak         us: English (Colemak)
-#    dvorak          us: English (Dvorak)
-# --- evdev.xml.orig	2019-11-04 14:19:29.608464564 +0100
-# +++ evdev.xml	2019-11-04 14:22:50.120291490 +0100
-# @@ -1375,6 +1375,12 @@
-#          </variant>
-#          <variant>
-#            <configItem>
-# +            <name>intl_nodeadkeys</name>
-# +            <description>English (US, intl., with no dead keys)</description>
-# +          </configItem>
-# +        </variant>
-# +        <variant>
-# +          <configItem>
-#              <name>alt-intl</name>
-#              <description>English (US, alt. intl.)</description>
-#            </configItem>
-# @@ -7388,4 +7394,4 @@
-#        </option>
-#      </group>
-#    </optionList>
-# -</xkbConfigRegistry>
-# \ No newline at end of file
-# +</xkbConfigRegistry>
+patch -p1 -d '/usr/share/X11/xkb/rules' << 'EOF'
+--- a/evdev.lst	2019-11-04 14:19:24.112470691 +0100
++++ b/evdev.lst	2019-11-04 14:20:24.040410777 +0100
+@@ -295,6 +295,7 @@
+   chr             us: Cherokee
+   euro            us: English (US, euro on 5)
+   intl            us: English (US, intl., with dead keys)
++  intl_nodeadkeys us: English (US, intl., with no dead keys)
+   alt-intl        us: English (US, alt. intl.)
+   colemak         us: English (Colemak)
+   dvorak          us: English (Dvorak)
+EOF
+
+patch -p1 -d '/usr/share/X11/xkb/rules' << 'EOF'
+--- a/evdev.xml	2019-11-04 14:19:29.608464564 +0100
++++ b/evdev.xml	2019-11-04 14:22:50.120291490 +0100
+@@ -1375,6 +1375,12 @@
+         </variant>
+         <variant>
+           <configItem>
++            <name>intl_nodeadkeys</name>
++            <description>English (US, intl., with no dead keys)</description>
++          </configItem>
++        </variant>
++        <variant>
++          <configItem>
+             <name>alt-intl</name>
+             <description>English (US, alt. intl.)</description>
+           </configItem>
+@@ -7388,4 +7394,4 @@
+       </option>
+     </group>
+   </optionList>
+-</xkbConfigRegistry>
+\ No newline at end of file
++</xkbConfigRegistry>
+EOF
