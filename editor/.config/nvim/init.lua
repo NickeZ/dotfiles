ba8aff699372fd0830529eda09ccf0603fd3ce90
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2024-08-29 12:36:44 +0200

    Add tmux config

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -144,8 +144,23 @@ require("lazy").setup({
                 }
             end
         },
-
-        -- add your plugins here
+        {
+            "christoomey/vim-tmux-navigator",
+            cmd = {
+                "TmuxNavigateLeft",
+                "TmuxNavigateDown",
+                "TmuxNavigateUp",
+                "TmuxNavigateRight",
+                "TmuxNavigatePrevious",
+            },
+            keys = {
+                { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+                { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+                { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+                { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+                { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+            },
+        },
     },
 })
 
