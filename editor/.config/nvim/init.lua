17bbf85c4dd9e4dcdbee43980b2004cad547f5c2
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2026-03-27 10:44:57 +0100

    wip

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -156,8 +156,7 @@ require("lazy").setup({
                 vim.o.background = 'dark'
                 -- XXX: hi Normal ctermbg=NONE
                 -- Make comments more prominent -- they are important.
-                local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
-                vim.api.nvim_set_hl(0, 'Comment', bools)
+                vim.api.nvim_set_hl(0, 'Comment', { italic = true, fg = 15, fg_indexed = true })
                 -- Make it clearly visible which argument we're at.
                 local marked = vim.api.nvim_get_hl(0, { name = 'PMenu' })
                 vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter',
@@ -335,8 +334,7 @@ require("lazy").setup({
                     vim.api.nvim_set_option_value("background", "dark", {})
                     vim.cmd("colorscheme gruvbox-dark-hard")
                     -- Make comments more prominent -- they are important.
-                    local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
-                    vim.api.nvim_set_hl(0, 'Comment', bools)
+                    vim.api.nvim_set_hl(0, 'Comment', { italic = true, fg = 15, fg_indexed = true })
                     -- Make it clearly visible which argument we're at.
                     local marked = vim.api.nvim_get_hl(0, { name = 'PMenu' })
                     vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter',
@@ -346,8 +344,7 @@ require("lazy").setup({
                     vim.api.nvim_set_option_value("background", "light", {})
                     vim.cmd("colorscheme gruvbox-light-hard")
                     -- Make comments more prominent -- they are important.
-                    local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
-                    vim.api.nvim_set_hl(0, 'Comment', bools)
+                    vim.api.nvim_set_hl(0, 'Comment', { italic = true, fg = 15, fg_indexed = true })
                     -- Make it clearly visible which argument we're at.
                     local marked = vim.api.nvim_get_hl(0, { name = 'PMenu' })
                     vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter',
@@ -400,6 +397,6 @@ require("lazy").setup({
                 "★",
                 "‒",
             },
-    	}
+        }
     }
 })
